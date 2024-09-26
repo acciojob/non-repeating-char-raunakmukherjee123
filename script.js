a68be2a056efa9e4d2543c4c80b0ec223150440f
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-09-26 16:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
+
+
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let map=new Map();
+	for(let i=0;i<str.length;i++)
+		{
+			let ch=str.charAt(i);
+			let freq=map.get(ch);
+			if(freq===undefined)
+			{
+				map.set(ch,1);
+			}
+			else
+			{
+				map.set(ch,freq+1);
+			}
+		}
+	for(let i=0;i<str.length;i++)
+		{
+			let ch=str.charAt(i);
+			let freq=map.get(ch);
+			if(freq==1)
+			{
+				return ch;
+			}
+		}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
